b1464788c3f9fa7188d74b0ddb12e620196e1a29
Theophile Thiberge <tthiberge@gmail.com>
2023-11-02 17:48:21 +0100

    Logo redirige vers home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,10 @@ import logo from '../data/logo_kasa.png'
 function Header() {
     return (
         <header>
+        <Link to="/" className="nav-item">
           <img src={logo} alt="logo kasa" className="logo" />
+        </Link>
+
           <nav className="navbar">
               <Link to="/" className="nav-item">Accueil</Link>
               <Link to="/about" className="nav-item">A propos</Link>

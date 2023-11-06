609acf7f22d0bfb48f170e650aeb5ccc6cf00907
Theophile Thiberge <tthiberge@gmail.com>
2023-11-06 19:59:47 +0100

    Call mentor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,16 @@ import logo from '../data/logo_kasa.png'
 function Header() {
     return (
         <header>
-        <Link to="/" className="nav-item">
-          <img src={logo} alt="logo kasa" className="logo" />
-        </Link>
+          <Link to="/" className="nav-item">
+            <img src={logo} alt="logo kasa" className="logo" />
+          </Link>
 
           <nav className="navbar">
               <Link to="/" className="nav-item">Accueil</Link>
               <Link to="/about" className="nav-item">A propos</Link>
           </nav>
         </header>
-    ) 
+    )
 }
 
 export default Header

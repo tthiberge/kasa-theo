8602ef7082eae1d283d62424d6611d51c3751170
Theophile Thiberge <tthiberge@gmail.com>
2023-11-10 10:11:27 +0100

    Design 404 et ajout fichiers

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom'
+import error_404 from '../data/404.png'
+
+
 function Error() {
   return (
-      <div>
-          <h1>Oups 🙈 Cette page n'existe pas</h1>
+      <div className="error-container">
+          <img src={error_404} alt="Error page" className='error-404' />
+          <p className='not-found'>Oups! La page que vous demandez n'existe pas.</p>
+          <Link to={`/`}>
+            <p className='error-to-home'>Retourner sur la page d’accueil</p>
+          </Link>
       </div>
   )
 }

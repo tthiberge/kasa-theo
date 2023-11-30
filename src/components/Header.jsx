36782151e1eac3f007e9fdb1af3c2dcceb8233d0
Theophile Thiberge <tthiberge@gmail.com>
2023-11-30 16:07:47 +0100

    Garder le soulignage sous les onglets actifs de navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import logo from '../data/logo_kasa.png'
 
 function Header() {
     return (
         <header>
-          <Link to="/" className="nav-item">
+          <NavLink to="/" className="nav-item">
             <img src={logo} alt="logo kasa" className="logo" />
-          </Link>
+          </NavLink>
 
           <nav className="navbar">
-              <Link to="/" className="nav-item">Accueil</Link>
-              <Link to="/about" className="nav-item">A propos</Link>
+              <NavLink to="/" className="nav-item">Accueil</NavLink>
+              <NavLink to="/about" className="nav-item">A propos</NavLink>
           </nav>
         </header>
     )
